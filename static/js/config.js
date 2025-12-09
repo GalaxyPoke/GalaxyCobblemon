5dbe8ed1479f48091ae4fc43eb0bd63ba4d4f8f3
maidang <maidang@txzing.com>
2025-12-09 19:18:50 +0800

    Update API URL to Render deployment

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,7 @@
 // API 配置
 // 本地开发时使用 localhost，部署后改成 Render 地址
 const API_BASE_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:3000'
+  ? 'https://galaxycobblemon-api.onrender.com'
   : 'https://你的render地址.onrender.com';  // 部署后修改这里
 
 // 导出配置
